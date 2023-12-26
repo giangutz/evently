43bdbf1060e9f7066719f10b7de66993321a8993
giangutz <80666756+giangutz@users.noreply.github.com>
2023-12-26 11:59:37 +0800

    complete header nav with clerk authentication

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Sportify",
+  description: "Sportify is a platform for sports management and analytics.",
+  icons: {
+    icon: "/public/assets/images/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
