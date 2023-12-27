cb5b029e2e799143e73dd67212297b6be669453b
giangutz <80666756+giangutz@users.noreply.github.com>
2023-12-27 20:49:39 +0800

    Update Header.tsx

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -8,14 +8,12 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <div className="w-full border-b">
+    <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <Image
-            src="/assets/images/logo.svg"
-            alt="Evently Logo"
-            width={128}
-            height={38}
+          <Image 
+            src="/assets/images/logo.svg" width={128} height={38}
+            alt="Evently logo" 
           />
         </Link>
 
@@ -25,19 +23,21 @@ const Header = () => {
           </nav>
         </SignedIn>
 
-        <div className="flex w-32  justify-end gap-3">
+        <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
             <MobileNav />
-            <SignedOut>
-              <Button className="rounded-full" size="lg">
-                <Link href="/sign-in">Login</Link>
-              </Button>
-            </SignedOut>
           </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
