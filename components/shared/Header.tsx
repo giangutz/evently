a97ba41738c8fc4787681d2b38fd37bfde13228d
giangutz <80666756+giangutz@users.noreply.github.com>
2023-12-27 20:59:07 +0800

    Updated clerk auth

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
             alt="Evently logo" 
           />
         </Link>
-
+ 
         <SignedIn>
           <nav className="md:flex-between hidden w-full max-w-xs">
             <NavItems />
