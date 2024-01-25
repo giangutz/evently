c89c7b63a6595c0a2f7ac508de02e398aecc8d5f
rr106544 <gian_gutierrez@dlsu.edu.ph>
2024-01-25 10:01:40 +0800

    webhook

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,21 +1,20 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import { Button } from "../ui/button";
 import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <div className="w-full border-b">
+    <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image
             src="/assets/images/logo.svg"
-            alt="Evently Logo"
             width={128}
             height={38}
+            alt="Evently logo"
           />
         </Link>
 
@@ -25,19 +24,19 @@ const Header = () => {
           </nav>
         </SignedIn>
 
-        <div className="flex w-32  justify-end gap-3">
+        <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
             <MobileNav />
-            <SignedOut>
-              <Button className="rounded-full" size="lg">
-                <Link href="/sign-in">Login</Link>
-              </Button>
-            </SignedOut>
           </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,36 +12,36 @@ import { CreateUserParams, UpdateUserParams } from '@/types'
 
 export async function createUser(user: CreateUserParams) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const newUser = await User.create(user)
-    return JSON.parse(JSON.stringify(newUser))
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
 export async function getUserById(userId: string) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const user = await User.findById(userId)
+    const user = await User.findById(userId);
 
     if (!user) throw new Error('User not found')
-    return JSON.parse(JSON.stringify(user))
+    return JSON.parse(JSON.stringify(user));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true });
 
-    if (!updatedUser) throw new Error('User update failed')
-    return JSON.parse(JSON.stringify(updatedUser))
+    if (!updatedUser) throw new Error('User update failed');
+    return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
     handleError(error)
   }
@@ -52,10 +52,10 @@ export async function deleteUser(clerkId: string) {
     await connectToDatabase()
 
     // Find user to delete
-    const userToDelete = await User.findOne({ clerkId })
+    const userToDelete = await User.findOne({ clerkId });
 
     if (!userToDelete) {
-      throw new Error('User not found')
+      throw new Error('User not found');
     }
 
     // Unlink relationships
